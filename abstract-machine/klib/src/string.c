c2db5fdd43a9d149116527da216d816447b0aa24
tracer-ics2024 <tracer@njuics.org>
2024-10-10 01:04:56 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:04:56 up  4:44,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -210,7 +210,35 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+
+  /* DESCRIPTION */
+  /*        The  memcmp()  function compares the first n bytes (each interpreted
+   * as unsigned char) of the memory */
+  /*        areas s1 and s2. */
+  /**/
+  /* RETURN VALUE */
+  /*        The memcmp() function returns an integer less than, equal to, or
+   * greater than zero if  the  first  n */
+  /*        bytes  of s1 is found, respectively, to be less than, to match, or
+   * be greater than the first n bytes */
+  /*        of s2. */
+  /**/
+  /*        For a nonzero return value, the sign is determined by the sign of
+   * the difference between  the  first */
+  /*        pair of bytes (interpreted as unsigned char) that differ in s1 and
+   * s2. */
+  /**/
+  /*        If n is zero, the return value is zero. */
+
+  const unsigned char *p1 = (const unsigned char *)s1;
+  const unsigned char *p2 = (const unsigned char *)s2;
+
+  for (size_t i = 0; i < n; i++) {
+    if (p1[i] != p2[i]) {
+      return p1[i] - p2[i];
+    }
+  }
+  return 0;
 }
 
 #endif
