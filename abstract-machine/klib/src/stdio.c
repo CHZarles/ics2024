51a7c04a9a701a9dcdcbbc89e57081365763b23e
tracer-ics2024 <tracer@njuics.org>
2024-10-10 03:01:46 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:01:46 up 26 min,  1 user,  load average: 0.09, 0.04, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,77 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+int printf(const char *fmt, ...) { panic("Not implemented"); }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // using vsnprintf to implement vsprintf
+  int ret = vsnprintf(out, 0x7fffffff, fmt, ap);
+  return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // using vsprintf to implement sprintf
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  // using vsnprintf to implement snprintf
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(out, n, fmt, ap);
+  return ret;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int i = 0;
+  for (; i < n && fmt[i] != '\0'; i++) {
+    // %s
+    if (fmt[i] == '%' && fmt[i + 1] == 's') {
+      char *str = va_arg(ap, char *);
+      int j = 0;
+      for (; j < strlen(str) && i < n; j++, i++) {
+        out[i] = str[j];
+      }
+      i--;
+    } else if (fmt[i] == '%' && fmt[i + 1] == 'd') {
+      int num = va_arg(ap, int);
+      char buf[20];
+      int j = 0;
+      if (num == 0) {
+        buf[j++] = '0';
+      } else {
+        if (num < 0) {
+          out[i++] = '-';
+          num = -num;
+        }
+        while (num > 0) {
+          buf[j++] = num % 10 + '0';
+          num /= 10;
+        }
+      }
+      for (int k = j - 1; k >= 0 && i < n; k--, i++) {
+        out[i] = buf[k];
+      }
+      i--;
+
+    } else {
+      out[i] = fmt[i];
+    }
+  }
+  if (i < n)
+    out[i] = '\0';
+  else
+    out[n - 1] = '\0';
+
+  return i;
+  /* panic("Not implemented"); */
 }
 
 #endif
