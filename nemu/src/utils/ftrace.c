db90bfd3bb01e62709d7b5e7475ea3c67d7b8e72
tracer-ics2024 <tracer@njuics.org>
2024-10-11 17:52:53 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:53 up 2 days,  1:00,  1 user,  load average: 0.20, 0.15, 0.13

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -72,7 +72,6 @@ void ftrace_ret_func(vaddr_t source_addr, vaddr_t target_addr) {
   Assert(func_stack_top > 0, "Function stack underflow");
   // display function return
   // 0x8000000c: ret [_trm_init@0x80000260]
-  printf("func_stack_top: %d\n", func_stack_top);
   printf("%x :", target_addr);
   for (int i = 0; i < (func_stack_top - 1) * 2; i++) {
     printf(" ");
