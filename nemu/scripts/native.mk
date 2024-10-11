8874795eb938553cc9f0e55a756f5044d33c7f51
tracer-ics2024 <tracer@njuics.org>
2024-10-11 11:36:40 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:36:40 up 1 day, 18:44,  1 user,  load average: 0.07, 0.05, 0.04

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -28,8 +28,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += --mtrace_log=$(BUILD_DIR)/nemu-mtrace.txt
 override ARGS += $(ARGS_DIFF)
 override ARGS += -b
-override ARGS += -elf_file=$(BUILD_DIR)/$(NAME).elf
-
+override ARGS += -elf_file=$(BUILD_DIR)/$(ARCHIEVE)
 
 # Command to execute NEMU
 IMG ?=
