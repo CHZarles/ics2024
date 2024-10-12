bb51ad0ff6610d8ccf3c7ac006448ea1a7d7c6fe
tracer-ics2024 <tracer@njuics.org>
2024-10-13 00:46:51 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:46:51 up  3:26,  1 user,  load average: 0.09, 0.03, 0.01

--- nemu/src/isa/mips32/difftest/dut.c
+++ nemu/src/isa/mips32/difftest/dut.c
@@ -18,13 +18,12 @@
 #include <isa.h>
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return true;
   // get the current state of the CPU
   Assert(pc == ref_r->pc, "pc mismatch");
   for (int i = 0; i < 32; i++) {
     Assert(ref_r->gpr[i] == gpr(i), "gpr[%d] mismatch", i);
   }
-  return true;
+  return false;
 }
 
 void isa_difftest_attach() {}
