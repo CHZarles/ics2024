5b70e3d81973908c90d1a7540490735f57e18680
tracer-ics2024 <tracer@njuics.org>
2024-10-13 21:46:48 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:46:48 up 20 min,  1 user,  load average: 0.13, 0.05, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -42,7 +42,7 @@ int printf(const char *fmt, ...) {
       }
     } else if (fmt[fmt_idx] == '%' && fmt[fmt_idx + 1] == 'x') { // hex
       fmt_idx += 2;
-      uint32_t num = va_arg(ap, uint32_t);
+      uint64_t num = va_arg(ap, uint64_t);
       char buf[20];
       int j = 0;
       if (num == 0) {
