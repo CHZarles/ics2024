eca6525b334121fcfebf1c89fb67cfe504837c87
tracer-ics2024 <tracer@njuics.org>
2024-10-14 16:30:04 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:04 up 6 min,  2 users,  load average: 0.74, 0.84, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,7 +14,7 @@ void __am_gpu_init() {
   printf("w = %d, h = %d\n", w, h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i++)
-    fb[i] = i;
+    fb[i] = 255;
   outl(SYNC_ADDR, 1);
 }
 
