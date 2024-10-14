31315a176d4630338680e508f8ef8fd175ef2748
tracer-ics2024 <tracer@njuics.org>
2024-10-14 17:02:14 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:02:14 up 39 min,  2 users,  load average: 0.15, 0.18, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -40,17 +40,29 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
    * 向屏幕(x, y)坐标处绘制w*h的矩形图像.
    * 图像像素按行优先方式存储在pixels中,
    * 每个像素用32位整数以00RRGGBB的方式描述颜色. */
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
   uint32_t *pixels = ctl->pixels;
-  uint32_t offset_x = ctl->x;
-  uint32_t offset_y = ctl->y;
-  for (int i = 0; i < ctl->w; ++i) {
-    for (int j = 0; j < ctl->h; ++j) {
-      uint32_t pixel = pixels[i * ctl->w + j];
-      int idx = (offset_x + i) + (offset_y + j) * ctl->w;
-      fb[idx] = pixel;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y + h; i++) {
+    for (int j = x; j < x + w; j++) {
+      fb[screen_w * i + j] = pixels[w * (i - y) + (j - x)];
     }
   }
+
+  /* uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; */
+  /* uint32_t *pixels = ctl->pixels; */
+  /* uint32_t offset_x = ctl->x; */
+  /* uint32_t offset_y = ctl->y; */
+  /* for (int i = 0; i < ctl->w; ++i) { */
+  /*   for (int j = 0; j < ctl->h; ++j) { */
+  /*     uint32_t pixel = pixels[i * ctl->w + j]; */
+  /*     int idx = (offset_x + i) + (offset_y + j) * ctl->w; */
+  /*     fb[idx] = pixel; */
+  /*   } */
+  /* } */
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
