e54b8e53f9788d366faf742cd0560a45df54e162
tracer-ics2024 <tracer@njuics.org>
2024-10-14 13:34:45 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:45 up 4 days, 20:42,  1 user,  load average: 0.13, 0.03, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,8 +4,8 @@
 void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t low32 = inl(RTC_ADDR);
   uint32_t high32 = inl(RTC_ADDR + 4);
+  uint32_t low32 = inl(RTC_ADDR);
   /* uint64_t time = ((uint64_t)high32 << 32) + (uint64_t)low32; */
   uptime->us = ((uint64_t)high32 << 32) | (uint64_t)low32;
   /* printf("low32: %d \n", low32); */
