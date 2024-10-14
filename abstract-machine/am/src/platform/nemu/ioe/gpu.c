29a62ca956b89cae2af591e32571ee00ba319577
tracer-ics2024 <tracer@njuics.org>
2024-10-14 17:11:25 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:25 up 48 min,  2 users,  load average: 0.30, 0.18, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,37 +3,18 @@
 #include <nemu.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-
-  /* int i; */
-  /* int w = 0; // TODO: get the correct width */
-  /* int h = 0; // TODO: get the correct height */
-  /* #define N 32 */
-  /*   int w = io_read(AM_GPU_CONFIG).width; */
-  /*   int h = io_read(AM_GPU_CONFIG).height; */
-  /*   printf("w = %d, h = %d\n", w, h); */
-  /*   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; */
-  /*   for (i = 0; i < w * h; i++) */
-  /*     fb[i] = i; */
-  /*   outl(SYNC_ADDR, 1); */
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   // read info rom VGACTL_ADDR
   uint32_t height = inw(VGACTL_ADDR);
   uint32_t width = inw(VGACTL_ADDR + 2);
-  printf("gpu config width = %d, height = %d\n", width, height);
+  /* printf("gpu config width = %d, height = %d\n", width, height); */
   *cfg = (AM_GPU_CONFIG_T){.present = true,
                            .has_accel = false,
                            .width = width,
                            .height = height,
                            .vmemsz = 0};
-
-  /* *cfg = (AM_GPU_CONFIG_T) { */
-  /*   .present = true, .has_accel = false, */
-  /*   .width = 0, .height = 0, */
-  /*   .vmemsz = 0 */
-  /* }; */
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
