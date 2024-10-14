fea820b4b8f1a595b66bb2239774e78acec0ddc3
tracer-ics2024 <tracer@njuics.org>
2024-10-14 17:20:33 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:33 up 57 min,  2 users,  load average: 0.66, 0.42, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -11,4 +11,8 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   if (kbd->keycode == AM_KEY_NONE) {
     kbd->keydown = false;
   }
+  int k = AM_KEY_NONE;
+  k = inl(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
