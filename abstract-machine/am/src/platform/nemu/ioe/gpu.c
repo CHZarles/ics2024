1e0c24578fc09f9951a58e75300150eb8b87e5a8
tracer-ics2024 <tracer@njuics.org>
2024-10-14 17:10:25 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:25 up 47 min,  2 users,  load average: 0.16, 0.14, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,11 +22,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   // read info rom VGACTL_ADDR
   uint32_t height = inw(VGACTL_ADDR);
   uint32_t width = inw(VGACTL_ADDR + 2);
-  /* printf("width = %d, height = %d\n", width, height); */
-  /* uint32_t screen_wh = inl(VGACTL_ADDR); */
-  /* height = screen_wh & 0xffff; */
-  /* width = screen_wh >> 16; */
-  printf("width = %d, height = %d\n", width, height);
+  printf("gpu config width = %d, height = %d\n", width, height);
   *cfg = (AM_GPU_CONFIG_T){.present = true,
                            .has_accel = false,
                            .width = width,
