02d830d1847e437742320f858c7eba5b2aad0e8b
tracer-ics2024 <tracer@njuics.org>
2024-10-14 16:47:36 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:36 up 24 min,  2 users,  load average: 0.02, 0.09, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -37,6 +37,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  /* AM帧缓冲控制器, 可写入绘图信息,
+   * 向屏幕(x, y)坐标处绘制w*h的矩形图像.
+   * 图像像素按行优先方式存储在pixels中,
+   * 每个像素用32位整数以00RRGGBB的方式描述颜色. */
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;
+  for (int i = 0; i < ctl->w; ++i) {
+    for (int j = 0; j < ctl->h; ++j) {
+      uint32_t pixel = pixels[i * ctl->h + j];
+      int x = ctl->x + i, y = ctl->y + j;
+      fb[x * ctl->h + y] = pixel;
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
