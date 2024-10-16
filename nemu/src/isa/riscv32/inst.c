a99f6d986c211d57c851014f8997a2e758f681e3
tracer-ics2024 <tracer@njuics.org>
2024-10-16 16:44:53 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:53 up 1 day,  2:33,  2 users,  load average: 0.09, 0.17, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,7 +138,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu, I,
           R(rd) = Mr(src1 + imm, 1));
 
-  INSTPAT("0000000 00000 00000 100 00000 11100 11", ecall, I,
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, I,
           printf("call ecall\n");
           s->dnpc = isa_raise_intr(0, s->pc);); // TODO: finish ecall
 
