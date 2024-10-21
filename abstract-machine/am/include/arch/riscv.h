336ef3c8826025468a1421430fd301e8924fbc1b
tracer-ics2024 <tracer@njuics.org>
2024-10-21 16:33:13 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:13 up 6 days,  2:21,  2 users,  load average: 0.25, 0.06, 0.02

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -19,9 +19,14 @@ struct Context {
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+// const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+//                       "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+//                       "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
+//                       "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+//
+#define GPR2 gpr[10] // a0
+#define GPR3 gpr[11] // a1
+#define GPR4 gpr[12] // a2
+#define GPRx gpr[10] // for ret, a0
 
 #endif
