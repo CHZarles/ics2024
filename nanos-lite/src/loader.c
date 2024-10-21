34e4157e3e372c039a46b8cc20619b367d2fedcf
tracer-ics2024 <tracer@njuics.org>
2024-10-21 11:59:30 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:59:30 up 5 days, 21:47,  2 users,  load average: 0.21, 0.19, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,6 +40,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr ph[ehdr.e_phnum];
   ramdisk_read(&ph, ehdr.e_phoff, sizeof(Elf_Phdr));
   printf("phnum = %d\n", phnum);
+  printf("self made\n");
   // 3.load program
   for (int i = 0; i < phnum; i++) {
     if (ph[i].p_type == PT_LOAD) {
