d249f174d4ef318852ae14c8384a456b9ca6dc63
tracer-ics2024 <tracer@njuics.org>
2024-10-21 16:02:37 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:02:37 up 6 days,  1:50,  2 users,  load average: 0.08, 0.03, 0.01

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,15 @@
 #include <common.h>
 
+// nano-lite 负责处理异常的回调函数
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_YIELD:
     Log("EVENT_YIELD");
     break;
   case EVENT_SYSCALL:
+    void do_syscall(Context * c);
     Log("EVENT_SYSCALL");
+    do_syscall(c);
     break;
   default:
     panic("Unhandled event ID = %d", e.event);
