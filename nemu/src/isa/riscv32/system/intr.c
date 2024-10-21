3e5dcb2512a3668e9f850cf47ebb7213cf43e9bc
tracer-ics2024 <tracer@njuics.org>
2024-10-21 15:33:27 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:27 up 6 days,  1:21,  2 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -28,6 +28,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   } else if (NO == 1) { // system call
 
     printf("system call\n");
+    panic("system NO = 1 , debug\n");
     cpu.csrs.mepc = epc;
   } else {
     cpu.csrs.mepc = epc;
