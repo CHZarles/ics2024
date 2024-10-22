684d793d90b4e8ba3c604b4b774c8d4bc911fad5
tracer-ics2024 <tracer@njuics.org>
2024-10-22 18:47:45 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:45 up  4:32,  2 users,  load average: 0.12, 0.08, 0.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,7 +16,7 @@ void strace(Context *c) {
     Log("write(%d, %s, %d)\n", c->GPR2, (char *)c->GPR3, c->GPR4);
     break;
   case 9: // SYS_brk
-    Log("brk(%d)\n", c->GPR2);
+    /* Log("brk(%d)\n", c->GPR2); */
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
