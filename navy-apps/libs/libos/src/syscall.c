49aee01c93115e7578c46ce6c126c687b8182898
tracer-ics2024 <tracer@njuics.org>
2024-10-22 11:40:33 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:33 up 6 days, 21:28,  2 users,  load average: 0.22, 0.30, 0.20

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -78,8 +78,6 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   /* _exit(SYS_write); */
-  printf("debug write(%d, %s, %d)\n", fd, (char *)buf, count);
-  exit(0);
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
