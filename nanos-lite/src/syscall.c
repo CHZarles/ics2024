06ae0f4b618cbdf5d87e3fcc7b8fb1aff7e347a5
tracer-ics2024 <tracer@njuics.org>
2024-10-22 11:27:45 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:45 up 6 days, 21:16,  2 users,  load average: 0.16, 0.13, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,24 @@ void do_syscall(Context *c) {
     yield();
     c->GPRx = 0;
     break;
+  case 4: // SYS_write
+    int ret = -1;
+    size_t len = c->GPR4;
+    // 检查fd的值, 如果fd是1或2(分别代表stdout和stderr)
+    int fd = c->GPR2;
+    char *buf = (char *)c->GPR3;
+    if (fd == 1 || fd == 2) // stdout , stderr
+    {
+      // 则将buf为首地址的len字节输出到串口(使用putch()即可).
+      for (size_t i = 0; i < len; i++) {
+        putch(buf[i]); // no return value
+      }
+      ret = len;
+    }
+    // 最后还要设置正确的返回值
+    c->GPRx = ret;
+    break;
+
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
