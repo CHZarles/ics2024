4922a4f72ce0177016f00156552a9b0fea44fbf0
tracer-ics2024 <tracer@njuics.org>
2024-10-22 20:03:22 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:03:22 up  5:48,  2 users,  load average: 0.02, 0.06, 0.07

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,7 +1,7 @@
 #include <fs.h>
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -11,7 +11,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -25,11 +25,49 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < LENGTH(file_table); i++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  return file_table[fd].read(buf, 0, len);
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  return file_table[fd].write(buf, 0, len);
+}
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence) {
+  case SEEK_SET:
+    break;
+  case SEEK_CUR:
+    offset += file_table[fd].disk_offset;
+    break;
+  case SEEK_END:
+    offset += file_table[fd].size;
+    break;
+  default:
+    panic("whence is invalid");
+  }
+  if (offset < 0 || offset > file_table[fd].size) {
+    return -1;
+  }
+  return file_table[fd].disk_offset = offset;
+}
+
+// 由于sfs没有维护文件打开的状态, fs_close()可以直接返回0, 表示总是关闭成功.
+int fs_close(int fd) { return 0; }
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
