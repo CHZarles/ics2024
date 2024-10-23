557f0a14527ec9a16669068043d565a468961974
tracer-ics2024 <tracer@njuics.org>
2024-10-23 16:21:54 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:54 up 1 day,  2:06,  2 users,  load average: 0.08, 0.04, 0.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,15 +68,15 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     return 0;
   }
   // special file
-  if (file_table[fd].write != invalid_write) {
-    return file_table[fd].write(buf, file_table[fd].open_offset, len);
-  }
-  /* if (fd == 1 || fd == 2) { */
-  /*   for (size_t i = 0; i < len; i++) { */
-  /*     putch(((char *)buf)[i]); */
-  /*   } */
-  /*   return len; */
+  /* if (file_table[fd].write != invalid_write) { */
+  /*   return file_table[fd].write(buf, file_table[fd].open_offset, len); */
   /* } */
+  if (fd == 1 || fd == 2) {
+    for (size_t i = 0; i < len; i++) {
+      putch(((char *)buf)[i]);
+    }
+    return len;
+  }
 
   size_t write_len = len;
   size_t open_offset = file_table[fd].open_offset;
