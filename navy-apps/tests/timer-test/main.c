8e5599120d2ae37a0d7085d835ee4c4dce5612a1
tracer-ics2024 <tracer@njuics.org>
2024-10-23 19:42:14 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:42:14 up 19 min,  1 user,  load average: 0.33, 0.14, 0.11

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+
+char buf[64];
+void double_to_string(double value) {
+  int int_part = (int)value;
+  double dec_part = value - int_part;
+  sprintf(buf, "%d.%d", int_part, (int)(dec_part * 1000000));
+  printf("%s\n", buf);
+}
 int main() {
   // 通过gettimeofday()获取当前时间, 并每过0.5秒输出一句话.
   struct timeval start, current;
@@ -10,15 +18,17 @@ int main() {
     long seconds = current.tv_sec - start.tv_sec;
     long microseconds = current.tv_usec - start.tv_usec;
     double elapsed = seconds + microseconds * 1e-6;
+
     /**/
-    printf("elapsed : %f\n", elapsed);
+    double_to_string(elapsed);
+    printf("elapsed : %s\n", buf);
     /* if (elapsed >= 1) { */
     /*   printf("0.5 seconds have passed\n"); */
     /*   gettimeofday(&start, NULL); // reset start time */
     /* } */
 
     /* usleep(10000); // sleep for 10 milliseconds */
-    for (int i = 0; i < 1000000000; ++i)
+    for (int i = 0; i < 10000000; ++i)
       ;
   }
 
