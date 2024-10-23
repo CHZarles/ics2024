c5b7c5658fd0f21404162b1025462971287e8e1f
tracer-ics2024 <tracer@njuics.org>
2024-10-23 19:44:25 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:44:25 up 21 min,  1 user,  load average: 0.29, 0.17, 0.12

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,13 +2,6 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-char buf[64];
-void double_to_string(double value) {
-  int int_part = (int)value;
-  double dec_part = value - int_part;
-  sprintf(buf, "%d.%d", int_part, (int)(dec_part * 1000000));
-  printf("%s\n", buf);
-}
 int main() {
   // 通过gettimeofday()获取当前时间, 并每过0.5秒输出一句话.
   struct timeval start, current;
@@ -29,7 +22,7 @@ int main() {
     /* } */
     printf("seconds");
     /* usleep(10000); // sleep for 10 milliseconds */
-    for (int i = 0; i < 10000000; ++i)
+    for (int i = 0; i < 100; ++i)
       ;
   }
 
