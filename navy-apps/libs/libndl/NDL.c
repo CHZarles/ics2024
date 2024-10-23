cf6bf70de4f5445a37284d6e8b33bcca35db0477
tracer-ics2024 <tracer@njuics.org>
2024-10-23 22:09:41 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:09:41 up  2:46,  1 user,  load average: 0.39, 0.13, 0.03

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
-
 uint32_t NDL_GetTicks() {
   struct timeval current;
   gettimeofday(&current, NULL);
