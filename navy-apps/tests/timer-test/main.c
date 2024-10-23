af3f1590a3f892526da9be256cf608f6c75e47d4
tracer-ics2024 <tracer@njuics.org>
2024-10-23 22:10:32 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:10:32 up  2:47,  1 user,  load average: 0.47, 0.19, 0.06

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -21,7 +21,7 @@ int main() {
       gettimeofday(&start, NULL); // reset start time
     }
     /* usleep(10000); // sleep for 10 milliseconds */
-    for (int i = 0; i < 100000; ++i)
+    for (int i = 0; i < 10000; ++i)
       ;
   }
 
