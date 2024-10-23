c7bd2863f442caa999d8688a86ce7a1ad094f95b
tracer-ics2024 <tracer@njuics.org>
2024-10-24 00:50:19 +0800

    >  compile NEMU 631220000 Charles Linux DESKTOP-L39GC67 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:50:19 up  5:27,  1 user,  load average: 0.07, 0.03, 0.00

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,11 +29,17 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   /* 松开按键事件, 如ku A表示松开A键 */
   AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
   if (kbd.keydown) {
+    size_t key = kbd.keycode;
     sprintf(buf, "kd %s", keyname[kbd.keycode]);
-  } else {
-    sprintf(buf, "ku %s", keyname[kbd.keycode]);
+    // wait for keyup
+    while (1) {
+      kbd = io_read(AM_INPUT_KEYBRD);
+      if (kbd.keydown == 0 && kbd.keycode == AM_KEY_NONE) {
+        sprintf(buf, "ku %s", keyname[key]);
+        break;
+      }
+    }
   }
-
   return 0;
 }
 
