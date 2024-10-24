cfa476bf0c2616e5638d68511561fbdde06d15c2
tracer-ics2024 <tracer@njuics.org>
2024-10-24 14:49:47 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:47 up 2 days, 34 min,  3 users,  load average: 0.29, 0.21, 0.18

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -35,7 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
     [FD_EVENT] = {"/dev/events", 0, 0, events_read, invalid_write}, // keyboard
     [FD_DISPLAY_INFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
-    [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write, 0}, // offset is zero
+    [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write}, // offset is zero
 // screen width and height
 #include "files.h"
 };
