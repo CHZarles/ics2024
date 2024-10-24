109af2b9c783e10920868209d059c9020cd9cb63
tracer-ics2024 <tracer@njuics.org>
2024-10-24 14:59:05 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:05 up 2 days, 43 min,  3 users,  load average: 0.28, 0.23, 0.19

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -6,10 +6,10 @@
 
 int main() {
   NDL_Init(0);
-  return 0;
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
+  return 0;
   NDL_OpenCanvas(&w, &h);
   printf("w = %d, h = %d\n", w, h);
   NDL_DrawRect(bmp, 0, 0, w, h);
