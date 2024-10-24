211a84019d51b37c2b8376caed3ea701d47892de
tracer-ics2024 <tracer@njuics.org>
2024-10-24 15:03:29 +0800

    >  compile NEMU 631220000 Charles Linux charles-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:29 up 2 days, 48 min,  3 users,  load average: 0.29, 0.37, 0.26

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -36,6 +36,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   // 打开一张(*w) X (*h)的画布
   // 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
   int sw, sh;
+  printf("buf = %s\n", buf);
   sscanf(buf, "WIDTH: %d\nHEIGHT: %d", screen_w, screen_h);
   printf("screen_w = %d, screen_h = %d\n", screen_w, screen_h);
   if (*w == 0 && *h == 0) {
